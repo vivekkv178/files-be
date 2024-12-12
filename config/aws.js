decdbae49580ae3e3c353c7f8d987950cdaf2abe
Vivek <vivekkv178@gmail.com>
2024-12-12 12:06:13 +0800

    fix: Build Issue fixed

--- config/aws.js
+++ config/aws.js
@@ -2,8 +2,8 @@ const AWS = require("aws-sdk");
 const ENV_CONFIG = require("./env-config");
 
 AWS.config.update({
-  accessKeyId: ENV_CONFIG.AWS_KEY_ID,
-  secretAccessKey: ENV_CONFIG.AWS_SECRET_KEY,
+  accessKeyId: ENV_CONFIG.AWS_ACCESS_KEY_ID,
+  secretAccessKey: ENV_CONFIG.AWS_SECRET_ACCESS_KEY,
   region: ENV_CONFIG.AWS_REGION,
 });
 
--- config/env-config.js
+++ config/env-config.js
@@ -1,6 +1,6 @@
 const ENV_CONFIG = {
-  AWS_KEY_ID: process.env.AWS_KEY_ID,
-  AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   AWS_REGION: process.env.AWS_REGION,
   MONGO_URL: process.env.MONGO_URL,
 };
