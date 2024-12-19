1421096999f28bfcad25d113c464ee6127a15cba
Vivek <vivekkv178@gmail.com>
2024-12-19 10:09:51 +0800

    feat: Server Side Pagination Impl

--- apis/file/get-file-data/service.js
+++ apis/file/get-file-data/service.js
@@ -5,12 +5,25 @@ const File = require("../models/file.model");
 
 module.exports = async (req, res, next) => {
   const fileId = req.params.fileId;
+  const { startRow, endRow } = req.query;
+
   try {
     const file = await File.findOne({ _id: fileId });
 
     const fileData = await FileData.findOne({ fileId });
 
-    res.status(200).json({ ...file.toObject(), ...fileData.toObject() });
+    if (!startRow) {
+      res.status(200).json({ ...file.toObject(), ...fileData.toObject() });
+    } else {
+      const totalRecords = fileData.data.length;
+      const paginatedData = fileData.data.slice(startRow, endRow);
+
+      res.json({
+        data: paginatedData,
+        fields: fileData.fields,
+        totalRecords,
+      });
+    }
   } catch (error) {
     console.error("Error fetching file data:", error);
     next(createError(500));
